033e55385a599d69408a0b6e4f29079ba5bc40e3
Geoff McQueen <geoff@geoff.space>
2026-03-14 03:31:18 -0700

    Remove main

--- test/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-int main(int argc, char** argv) {
-    //GTEST_FLAG_SET(death_test_style, "threadsafe");
-    GTEST_FLAG_SET(death_test_style, "fast");
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
